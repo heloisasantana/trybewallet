a0ae47cac358bed0e13eeb926f6764a1c03cf4cb
heloisasantana <heloisasantana.silva@hotmail.com>
2022-07-28 18:02:42 -0300

    Requisito 1 incompleto

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
-// Iniciando o Projeto!
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from './pages/Login'
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <div>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={ Login } />
+          </Switch>
+        </BrowserRouter>
+      </div>
+  )
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,60 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+  super();
+  this.state = {
+    validEmail: false,
+    validPassword: false,
+    disabled: true,
+  }
+  }
+  
+  handleEmail = (event) => {
+    if (event.target.type === 'email') {
+      const email = event.target.value;
+      // Referência utilizada para uso do regex: https://stackoverflow.com/questions/35788383/regex-validation-in-javascript-email;
+      const rule = /\S+@\S+\.\S+/;
+      console.log(rule.test(email)); 
+      this.setState({ validEmail: rule.test(email) });
+      return rule.test(email);
+    }
+  }
+
+  handlePassword = (event) => {
+    if (event.target.type === 'password') {
+    const { validPassword } = this.state;
+    const password = event.target.value;
+    const min = 6; 
+    if (password.length >= min) { this.setState({ validPassword: true })}
+    console.log(validPassword); 
+    return validPassword;
+  }
+}
+ 
+  validateButton = (event) => {
+    console.log('validateButton funcionando');
+    this.handleEmail(event);
+    this.handlePassword(event);
+    const { validEmail, validPassword } = this.state;
+    console.log(validEmail);
+    console.log(validPassword);
+    console.log(validEmail && validPassword);
+    if (validEmail && validPassword === true) {
+      this.setState({ disabled: false })
+    }
+    else { this.setState({ disabled: true }) }
+  }
+
   render() {
-    return <div>Login</div>;
+    const { disabled } = this.state;
+    return (
+    <div>
+      <input type="email" data-testid="email-input" onChange={ this.validateButton } />
+      <input type="password" data-testid="password-input" onChange={ this.validateButton } />
+      <input type="button" value="Entrar" disabled={ disabled } />
+    </div>
+    );
   }
 }
 
