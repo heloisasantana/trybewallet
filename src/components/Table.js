20c0d82a12dd002ae8b4bd5b1b5573e6beb3f9d2
heloisasantana <heloisasantana.silva@hotmail.com>
2022-08-01 21:09:22 -0300

    Requisito 7 completo

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,21 +1,56 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Table extends React.Component {
   render() {
+    const { expenses } = this.props;
     return (
       <div>
-        <th>Descrição</th>
-        <th>Tag</th>
-        <th>Método de pagamento</th>
-        <th>Valor</th>
-        <th>Moeda</th>
-        <th>Câmbio utilizado</th>
-        <th>Valor convertido</th>
-        <th>Moeda de conversão</th>
-        <th>Editar/Excluir</th>
+        <table>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+          { expenses.map((e) => (
+            <tbody key={ e.id }>
+              <tr key={ e.id }>
+                <td>{ e.description }</td>
+                <td>{ e.tag }</td>
+                <td>{ e.method }</td>
+                <td>{ Number(e.value).toFixed(2) }</td>
+                <td>{ e.exchangeRates[e.currency].name }</td>
+                <td>{ Number(e.exchangeRates[e.currency].ask).toFixed(2) }</td>
+                <td>
+                  { (Number(e.value) * Number(e.exchangeRates[e.currency].ask))
+                    .toFixed(2) }
+                </td>
+                <td>Real</td>
+              </tr>
+            </tbody>
+          ))}
+        </table>
       </div>
     );
   }
 }
 
-export default Table;
+function mapStateToProps(state) {
+  return {
+    expenses: state.wallet.expenses,
+  };
+}
+
+Table.propTypes = {
+  // Referência para utilizar o any para receber um objeto com qualquer tipo de dado: https://www.codegrepper.com/code-examples/whatever/proptypes.any;
+  expenses: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.any)).isRequired,
+};
+
+export default connect(mapStateToProps, null)(Table);
